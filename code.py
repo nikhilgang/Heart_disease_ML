6ab132c291ee134c462546d574ebb5903ab2594e
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2020-10-03 14:05:12 +0530

    loading data

---
+++ code.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+
+df = pd.read_csv("/content/drive/My Drive/ML_work/UCI_dataset.csv")
+
+df.head()
+
