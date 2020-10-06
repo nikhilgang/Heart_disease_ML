d03b59884449137fce5daf1178b5b317a85eea13
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-10-06 17:07:11 +0530

    Update data exploration.py

--- data exploration.py
+++ data exploration.py
@@ -11,3 +11,18 @@ print("Percentage of Patients Have Heart Disease: {:.2f}%".format((countHaveDise
 sns.countplot(x='sex', data=df, palette="mako_r")
 plt.xlabel("Sex (0 = female, 1= male)")
 plt.show()
+
+
+countFemale = len(df[df.sex == 0])
+countMale = len(df[df.sex == 1])
+print("Percentage of Female Patients: {:.2f}%".format((countFemale / (len(df.sex))*100)))
+print("Percentage of Male Patients: {:.2f}%".format((countMale / (len(df.sex))*100)))
+
+df.groupby('target').mean()
+
+pd.crosstab(df.age,df.target).plot(kind="bar",figsize=(20,6))
+plt.title('Heart Disease Frequency for Ages')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.savefig('heartDiseaseAndAges.png')
+plt.show()
